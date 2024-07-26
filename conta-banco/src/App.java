b4788f086d43d78f69adb131254d2b8d5a2a1d9a
C1321182 Alef Henrique Das Chagas <c1321182@interno.bb.com.br>
2024-07-25 22:57:47 -0300

    desafio conta terminal

--- conta-banco/src/App.java
+++ conta-banco/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua conta");
+        double conta = scanner.nextInt();
+
+        System.out.println("Digite sua Agencia");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Ola " + nomeCliente + " Obrigado por criar sua conta em nosso banco, sua agencia é "
+                + agencia + " conta " + conta + " e seu saldo " + saldo + " ja esta disponivel para saque");
     }
 }
